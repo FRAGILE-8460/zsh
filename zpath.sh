7d0fbd7c1583ffc37f16ae3ca4a92f271f4a7106
Takashi.Takehana <github@racing-spirit.homes>
2026-02-07 12:59:12 +0900

    パス設定の一元化

---
+++ zpath.sh
@@ -0,0 +1,22 @@
+# zpath.sh
+
+# PATH
+# Zshのpath配列を使うと、より安全で管理がしやすい
+# 下に追加するほど、優先的に使われる
+path=(
+  /opt/homebrew/sbin
+  /opt/homebrew/opt/llvm/bin
+  /Users/takashi/tools/arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi/bin
+  $path # 既存のPATHを引き継ぐ
+)
+
+# fpath (補完関数のパス)
+# 下に追加するほど、優先的に使われる
+fpath=(
+  /Users/takashi/.docker/completions
+  $fpath # 既存のfpathを引き継ぐ
+)
+
+# STM32
+export STM32_PRG_PATH=/Applications/STMicroelectronics/STM32Cube/STM32CubeProgrammer/STM32CubeProgrammer.app/Contents/MacOs/bin
+export STM32CubeMX_PATH=/Applications/STMicroelectronics/STM32CubeMX.app/Contents/Resources
--- zprofile.sh
+++ zprofile.sh
@@ -2,4 +2,5 @@ if [ -f "$HOME/ZSH/.secrets" ]; then
     source "$HOME/ZSH/.secrets"
 fi
 
-export PATH="$PATH:/Users/takashi/tools/arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi/bin"
+# PATH設定はzpath.shに分離
+source "$HOME/ZSH/zpath.sh"

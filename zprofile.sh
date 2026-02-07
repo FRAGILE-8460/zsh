ef9a0f1a9146beb3364330e38a3056dd8a5a9934
Takashi.Takehana <github@racing-spirit.homes>
2026-02-07 12:49:16 +0900

    fix typo

--- zprofile.sh
+++ zprofile.sh
@@ -2,4 +2,4 @@ if [ -f "$HOME/ZSH/.secrets" ]; then
     source "$HOME/ZSH/.secrets"
 fi
 
-export PATH = "$PATH:/Users/takashi/tools/arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi/bin"
+export PATH="$PATH:/Users/takashi/tools/arm-gnu-toolchain-14.2.rel1-darwin-arm64-arm-none-eabi/bin"
